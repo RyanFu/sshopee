1e41641ec2622597144b18f069a59e1364b70262
anywheretogo <guoliang@elenxs.com>
2021-02-18 11:16:59 +0800

    ~~

--- shopee_api.py
+++ shopee_api.py
@@ -153,7 +153,7 @@ def get_performance(account):
 def get_all_performance():
     sql = 'select account from password'
     cu = mydb(sql)
-    account_list = [i for i in cu]
+    account_list = [i[0] for i in cu]
     for account in account_list:
         check_cookie_jar(account)
     multiple_mission_pool(get_performance, account_list, 10)
