1d15114ed5b5f5692a0ccb6c3580a0bbff178a6b
anywheretogo <63397520@qq.com>
2021-05-12 00:45:55 +0800

    添加打卡

--- app.py
+++ app.py
@@ -137,6 +137,10 @@ def consolePage():
 def collectionPage():
     return render_template("collection.html")
 
+@app.route('/steps', methods = ['GET'])
+def stepPage():
+    return render_template("step.html")
+
 #前台初始化
 @app.route('/basic_info', methods = ['GET'])
 def basic_info():
